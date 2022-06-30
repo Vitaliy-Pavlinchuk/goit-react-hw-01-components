37a59403a1dcde4eee88aa8db8547c789827ed3b
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-06-30 11:46:08 +0300

    finish-fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,10 +7,10 @@ export const Statistics = ({ title, stats }) => {
       {title && <h2 className={styles.title}>{title}</h2>}
 
       <ul className={styles.statsList}>
-        {stats.map(item => (
-          <li className={styles.item} key={item.id}>
-            <span className={styles.label}>{item.label}</span>
-            <span className={styles.percentage}>{item.percentage}</span>
+        {stats.map(({ id, label, percentage }) => (
+          <li className={styles.item} key={id}>
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}</span>
           </li>
         ))}
       </ul>
@@ -20,11 +20,11 @@ export const Statistics = ({ title, stats }) => {
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  item: PropTypes.arrayOf(
+  stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
-    })
-  ),
+    }).isRequired
+  ).isRequired,
 };
